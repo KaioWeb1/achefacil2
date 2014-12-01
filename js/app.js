70b1521fa9ad6c5b184469c96e445867e25491cb
KaioWeb <kaio.sistemasweb@gmail.com>
2014-12-01 15:08:38 -0200

    novos arquivos

--- js/app.js
+++ js/app.js
@@ -19,6 +19,54 @@ var achefacil = angular.module("achefacil",['ngRoute']).config(function($routePr
 		.when("/adv/",{
 			templateUrl:"templates/advocacia.html",
 			controller:"Adv"
+		})
+		.when("/adv/:id",{
+			templateUrl:"templates/empresa.html",
+			controller:"AdvId"
+		})
+		.when("/animais/",{
+			templateUrl:"templates/animais.html",
+			controller:"Animais"
+		})
+		.when("/auto/",{
+			templateUrl:"templates/auto.html",
+			controller:"Auto"
+		})
+		.when("/casa/",{
+			templateUrl:"templates/casa.html",
+			controller:"Casa"
+		})
+		.when("/contabilidade/",{
+			templateUrl:"templates/contabilidade.html",
+			controller:"Contabilidade"
+		})
+		.when("/diversos/",{
+			templateUrl:"templates/diversos.html",
+			controller:"Diversos"
+		})
+		.when("/educacao/",{
+			templateUrl:"templates/educacao.html",
+			controller:"Educacao"
+		})
+		.when("/entretenimento/",{
+			templateUrl:"templates/entretenimento.html",
+			controller:"Entrenimento"
+		})
+		.when("/gastronomia/",{
+			templateUrl:"templates/gastronomia.html",
+			controller:"Gastronomia"
+		})
+		.when("/livrarias/",{
+			templateUrl:"templates/livrarias.html",
+			controller:"Livrarias"
+		})
+		.when("/moda/",{
+			templateUrl:"templates/moda.html",
+			controller:"Moda"
+		})
+		.when("/saude/",{
+			templateUrl:"templates/saude.html",
+			controller:"Saude"
 		}).otherwise({
 			redirectTo:"/categorias"	
 		});
@@ -26,7 +74,6 @@ var achefacil = angular.module("achefacil",['ngRoute']).config(function($routePr
 }).run(function () {
     FastClick.attach(document.body);
 });
-
 
 achefacil.controller('Adv', function($scope, Adv, $window){
 		$scope.adv = {}
@@ -36,8 +83,141 @@ achefacil.controller('Adv', function($scope, Adv, $window){
 	$scope.doTheBack = function() {
 		  window.history.back();
 	};
+});
+
+achefacil.controller('AdvId', function($scope,$filter ,$routeParams,$window, Adv){
+	$scope.id = $routeParams.id;
+	var myfilter = $filter;
+	Adv.getAdv(function(data){
+		$scope.adv = myfilter('filter')(data,{
+			id:$routeParams.id
+		})[0];	
+	})
+
+	$scope.doTheBack = function() {
+	  window.history.back();
+	};
 
 });
+
+
+achefacil.controller('Animais', function($scope, Animais, $window){
+		$scope.animais = {}
+		Animais.getAnimais(function(data){			
+			$scope.animais = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Auto', function($scope, Auto, $window){
+		$scope.auto = {}
+		Auto.getAuto(function(data){			
+			$scope.Auto = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Casa', function($scope, Casa, $window){
+		$scope.casa = {}
+		Casa.getCasa(function(data){			
+			$scope.casa = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Contabilidade', function($scope, Contabilidade, $window){
+		$scope.contabilidade = {}
+		Contabilidade.getContabilidade(function(data){			
+			$scope.contabilidade = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Diversos', function($scope, Diversos, $window){
+		$scope.diversos = {}
+		Diversos.getDiversos(function(data){			
+			$scope.diversos = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Educacao', function($scope, Educacao, $window){
+		$scope.educacao = {}
+		Educacao.getEducacao(function(data){			
+			$scope.educacao = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Entrenimento', function($scope, Entrenimento, $window){
+		$scope.entretenimento = {}
+		Entrenimento.getEntretenimento(function(data){			
+			$scope.entretenimento = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Gastronomia', function($scope, Gastronomia, $window){
+		$scope.gastronomia = {}
+		Gastronomia.getGastronomia(function(data){			
+			$scope.gastronomia = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Livrarias', function($scope, Livrarias, $window){
+		$scope.livrarias = {}
+		Livrarias.getLivrarias(function(data){			
+			$scope.livrarias = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Moda', function($scope, Moda, $window){
+		$scope.moda = {}
+		Moda.getModa(function(data){			
+			$scope.moda = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Saude', function($scope, Saude, $window){
+		$scope.saude = {}
+		Saude.getSaude(function(data){			
+			$scope.saude = data;	
+		})
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+achefacil.controller('Servicos', function($scope, Servicos, $window, $routeParams){
+		$scope.servicos = {}
+		Servicos.getServicos(function(data){			
+			$scope.servicos = data;	
+		})
+	$scope.servicos.currentCategoria = null;
+	function setCurrentCategoria(categoria){
+		console.log("setCurrentCategoria "+ categoria);
+		$scope.servicos.currentCategoria = categoria;
+	}
+	$scope.servicos.setCurrentCategoria = setCurrentCategoria;
+	$scope.doTheBack = function() {
+		  window.history.back();
+	};
+});
+
+
+
 
 achefacil.controller('Categoria', function($scope, Categoria){
 	
@@ -50,31 +230,7 @@ achefacil.controller('Categoria', function($scope, Categoria){
 
 });
 
-achefacil.controller('Servicos', function($scope, Servicos, $window, $routeParams){
-		$scope.servicos = {}
-		Servicos.getServicos(function(data){			
-			$scope.servicos = data;	
-		})
 
-	$scope.servicos.currentCategoria = null;
-		
-	
-
-	
-
-	function setCurrentCategoria(categoria){
-		console.log("setCurrentCategoria "+ categoria);
-		$scope.servicos.currentCategoria = categoria;
-	}
-
-	$scope.servicos.setCurrentCategoria = setCurrentCategoria;
-
-
-	$scope.doTheBack = function() {
-		  window.history.back();
-	};
-
-});
 
 achefacil.controller('Empresa', function($scope,$filter ,$routeParams,$window, Servicos){
 	$scope.id = $routeParams.id;
@@ -90,6 +246,9 @@ achefacil.controller('Empresa', function($scope,$filter ,$routeParams,$window, S
 	};
 
 });
+
+
+
 
 
 achefacil.controller('Sobre', function($scope){
@@ -186,6 +345,339 @@ achefacil.factory('Adv', function($http){
 		},
 
 		saveAdv:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Animais', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getAnimais:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/animais.json'
+				}).success(function(data){
+					obj.saveAnimais(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveAnimais:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+
+achefacil.factory('Auto', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getAuto:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/auto.json'
+				}).success(function(data){
+					obj.saveAuto(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveAuto:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Casa', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getCasa:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/casa.json'
+				}).success(function(data){
+					obj.saveCasa(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveCasa:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Contabilidade', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getContabilidade:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/contabilidade.json'
+				}).success(function(data){
+					obj.saveContabilidade(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveContabilidade:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Diversos', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getDiversos:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/diversos.json'
+				}).success(function(data){
+					obj.saveDiversos(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveDiversos:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Educacao', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getEducacao:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/educacao.json'
+				}).success(function(data){
+					obj.saveEducacao(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveEducacao:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Entrenimento', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getEntretenimento:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/entretenimento.json'
+				}).success(function(data){
+					obj.saveEntretenimento(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveEntretenimento:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+
+achefacil.factory('Gastronomia', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getGastronomia:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/gastronomia.json'
+				}).success(function(data){
+					obj.saveGastronomia(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveGastronomia:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+
+achefacil.factory('Livrarias', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getLivrarias:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/livrarias.json'
+				}).success(function(data){
+					obj.saveLivrarias(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveLivrarias:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Moda', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getModa:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/moda.json'
+				}).success(function(data){
+					obj.saveModa(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveModa:function(data){
+			servicosList = data;
+		}
+		
+	}
+
+
+	return obj;
+})
+achefacil.factory('Saude', function($http){
+
+	var servicosList;
+	var obj = {};
+
+	obj = {
+		getSaude:function(callback){
+			if (servicosList){
+				callback(servicosList);
+				return false;
+			}else{
+				$http({
+					method:'GET',
+					url:'data/saude.json'
+				}).success(function(data){
+					obj.saveSaude(data);
+					callback(data);	
+				});
+			}
+		},
+
+		saveSaude:function(data){
 			servicosList = data;
 		}
 		
